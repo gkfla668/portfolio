5ebf80a72a8ebc21f3b2a34314d98860e3d5a050
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-01 20:10:23 +0900

    design(Footer): 폰트 수정

--- limhalim/components/Layout/Footer.tsx
+++ limhalim/components/Layout/Footer.tsx
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  font-family: "Noto Sans KR", sans-serif;
-
+const Container = styled.div`
   width: 100%;
   padding: 3.2rem;
 
-  background-color: #42d17ee8;
+  background-color: #42d17ee6;
 
   display: flex;
   flex-direction: column;
@@ -17,23 +15,25 @@ const Wrapper = styled.div`
 `;
 
 const Title = styled.h1`
-  font-size: 1.2rem;
+  font-family: "NanumSquareBold";
+  font-size: 1.4rem;
   color: white;
 `;
 
 const Text = styled.p`
+  font-family: "NanumSquareBold";
   font-size: 1rem;
-  color: #ffffffb7;
+  color: #ffffffd7;
 `;
 
 const Footer = () => {
   return (
-    <Wrapper>
+    <Container>
       <Title>
         본 페이지는 상업적 목적이 아닌, 개인 포트폴리오용으로 제작되었습니다.
       </Title>
       <Text>© 2024 Lim, Ha-Lim. All rights Reserved.</Text>
-    </Wrapper>
+    </Container>
   );
 };
 
