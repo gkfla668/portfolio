ddde57f3f54ec26bbda1d0b4ddef7960fdafdf0f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-01 20:11:36 +0900

    feat: Home, Main Page 렌더링

---
+++ limhalim/pages/main/index.tsx
@@ -0,0 +1,53 @@
+import AboutPage from "../../components/About";
+import SkillsPage from "../../components/Skills";
+import ProjectsPage from "../../components/Projects";
+import ContactPage from "../../components/Contact";
+
+import Bubble from "@/components/Bubble";
+import Contents from "@/components/TableOfContents";
+import Footer from "@/components/Layout/Footer";
+
+import HandleWheel from "@/utils/handleWheel";
+
+const Main = () => {
+  const sections = ["Contents", "About", "Skills", "Projects", "Contact"];
+
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <Bubble />
+
+      <div className="z-10 flex w-full flex-col">
+        {sections.map((section, index) => (
+          <div
+            key={section}
+            id={section}
+            onWheel={e => HandleWheel(e, index, sections)}
+          >
+            {getPageComponent(section)}
+          </div>
+        ))}
+      </div>
+
+      <Footer />
+    </main>
+  );
+};
+
+const getPageComponent = (section: string) => {
+  switch (section) {
+    case "Contents":
+      return <Contents />;
+    case "About":
+      return <AboutPage />;
+    case "Skills":
+      return <SkillsPage />;
+    case "Projects":
+      return <ProjectsPage />;
+    case "Contact":
+      return <ContactPage />;
+    default:
+      return;
+  }
+};
+
+export default Main;
