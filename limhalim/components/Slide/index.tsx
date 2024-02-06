4e872f2b6da8db5dbd9a9f814f2a03dac769cc6c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-06 18:30:24 +0900

    feat: Projects 자동 재생 추가 및 이미지 경로 변경

--- limhalim/components/Slide/index.tsx
+++ limhalim/components/Slide/index.tsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import HandleScroll from "@/utils/handleScroll";
-
 import SlideContainer from "./SlideContainer";
-
-import MeQ from "../../public/images/MeQ.png";
-import DamDam from "../../public/images/Damdam.png";
-import StockOneQ from "../../public/images/StockOneQ.png";
-import LivePick from "../../public/images/LivePick.png";
-import Wanted from "../../public/images/WantedClone.png";
 
 import { dataById } from "@/data/projects";
 
@@ -22,6 +13,8 @@ const Slide = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true, // 자동 재생 활성화
+    autoplaySpeed: 4000,
   };
 
   return (
@@ -29,35 +22,35 @@ const Slide = () => {
       <Slider {...settings} className="mx-20 ">
         <SlideContainer
           index={1}
-          imgURL={MeQ}
+          imgURL={dataById[1].imgURL}
           title={dataById[1].title}
           subTitle={dataById[1].subTitle}
           tagList={dataById[1].tagList}
         />
         <SlideContainer
           index={2}
-          imgURL={DamDam}
+          imgURL={dataById[2].imgURL}
           title={dataById[2].title}
           subTitle={dataById[2].subTitle}
           tagList={dataById[2].tagList}
         />
         <SlideContainer
           index={3}
-          imgURL={StockOneQ}
+          imgURL={dataById[3].imgURL}
           title={dataById[3].title}
           subTitle={dataById[3].subTitle}
           tagList={dataById[3].tagList}
         />
         <SlideContainer
           index={4}
-          imgURL={LivePick}
+          imgURL={dataById[4].imgURL}
           title={dataById[4].title}
           subTitle={dataById[4].subTitle}
           tagList={dataById[4].tagList}
         />
         <SlideContainer
           index={5}
-          imgURL={Wanted}
+          imgURL={dataById[5].imgURL}
           title={dataById[5].title}
           subTitle={dataById[5].subTitle}
           tagList={dataById[5].tagList}
