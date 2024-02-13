0df49ed1922221ebc6b9069ae012e69c80033a52
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-13 20:37:54 +0900

    slide props type 추가

---
+++ limhalim/types/global.d.ts
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image";
+
+export interface SlideProps {
+  index: number;
+  imgURL: StaticImageData;
+  title: string;
+  subTitle?: string;
+  tagList: string[];
+}
+
+export interface DetailProps extends SlideProps {
+  githubURL: string;
+  siteURL: string;
+  overView: string;
+  infoList: string[];
+}
