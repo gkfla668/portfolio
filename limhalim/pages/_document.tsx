126483e6f98bc10fab872a4b73d75c40116eed41
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-13 20:37:21 +0900

    font link 추가

--- limhalim/pages/_document.tsx
+++ limhalim/pages/_document.tsx
@@ -4,6 +4,37 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Noto+Sans+KR:wght@500&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Libre+Barcode+128+Text&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rubik+Burned&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
+        rel="stylesheet"
+      ></link>
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square.css"
+        rel="stylesheet"
+      ></link>
       <body>
         <Main />
         <NextScript />
