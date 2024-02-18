5311e9978b1acbaa1c7ea3a4f8811a3814956926
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-18 18:12:03 +0900

    styled.ts 파일 삭제

--- limhalim/pages/detail/[id].tsx
+++ limhalim/pages/detail/[id].tsx
@@ -1,10 +1,152 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import * as S from "./styled";
 
 import { dataById } from "@/data/projects";
 import Bubble from "@/components/Bubble";
 import { useEffect, useState } from "react";
+
+import styled from "styled-components";
+
+export const Title = styled.div`
+  font-weight: 900;
+  color: White;
+  font-size: 3.6rem;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    font-size: 2.8rem;
+  }
+`;
+
+export const SubTitle = styled.div`
+  font-weight: 900;
+  color: White;
+  opacity: 0.6;
+  font-size: 2.4rem;
+
+  @media (max-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+export const TagList = styled.ul`
+  display: flex;
+  gap: 1rem;
+
+  margin-top: 1.4rem;
+
+  @media (max-width: 768px) {
+    gap: 0.4rem;
+  }
+`;
+
+export const TagItem = styled.li`
+  border-radius: 2rem;
+  border: 1px solid #42d17d;
+
+  padding: 0.6rem 1.6rem;
+
+  color: #42d17d;
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  letter-spacing: 0.1rem;
+
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    font-size: 1.1rem;
+  }
+`;
+
+export const Link = styled.a`
+  cursor: pointer;
+
+  border-radius: 2.4rem;
+
+  color: #42d17d;
+  background-color: #42d17e29;
+  font-size: 1.4rem;
+  font-weight: 900;
+  white-space: nowrap;
+
+  &:hover {
+    color: #363636;
+    background-color: #42d17d;
+    font-weight: 900;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.3rem;
+  }
+`;
+
+export const OverViewText = styled.div`
+  color: white;
+  opacity: 0.8;
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+  margin-left: 1rem;
+
+  @media (max-width: 768px) {
+    font-size: 1.4rem;
+    line-height: 2.1rem;
+  }
+`;
+
+export const SubText = styled.div`
+  color: white;
+  font-size: 2.6rem;
+  font-weight: 900;
+
+  padding: 1.2rem 0rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+    padding: 0.6rem 0rem;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+
+  width: 100%;
+
+  @media (max-width: 768px) {
+    gap: 0.8rem;
+  }
+`;
+
+export const InfoTitle = styled.div`
+  border-radius: 2.4rem;
+  width: 14rem;
+  white-space: nowrap;
+  padding: 0.8rem 1rem;
+
+  color: #42d17d;
+  font-weight: 900;
+  background-color: #42d17e29;
+  font-size: 1.3rem;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+    padding: 0.6rem 0rem;
+  }
+`;
+
+export const InfoText = styled.div`
+  color: white;
+  font-size: 1.4rem;
+
+  display: flex;
+  width: 100%;
+  gap: 0.4rem;
+
+  white-space: nowrap;
+`;
 
 const DetailPage = () => {
   const router = useRouter();
@@ -29,27 +171,24 @@ const DetailPage = () => {
         ? dataById[parsedId] && (
             <div className="flex w-[80%] flex-col items-center justify-center">
               <div className="flex w-full flex-col gap-2">
-                <S.Title>{dataById[parsedId].title}</S.Title>
-                <S.SubTitle>{dataById[parsedId].subTitle}</S.SubTitle>
-                <S.TagList>
+                <Title>{dataById[parsedId].title}</Title>
+                <SubTitle>{dataById[parsedId].subTitle}</SubTitle>
+                <TagList>
                   {dataById[parsedId].tagList.map((item, index) => (
-                    <S.TagItem key={index}>{`#${item}`}</S.TagItem>
+                    <TagItem key={index}>{`#${item}`}</TagItem>
                   ))}
-                </S.TagList>
+                </TagList>
               </div>
               <div className="mb-8 mt-20 flex w-full items-center justify-center gap-3 md:mt-24 md:gap-4">
-                <S.Link
+                <Link
                   href={dataById[parsedId].githubURL}
                   className="px-10 py-4"
                 >
                   깃허브 바로가기
-                </S.Link>
-                <S.Link
-                  href={dataById[parsedId].siteURL}
-                  className="px-10 py-4"
-                >
+                </Link>
+                <Link href={dataById[parsedId].siteURL} className="px-10 py-4">
                   사이트 바로가기
-                </S.Link>
+                </Link>
               </div>
 
               <Image
@@ -60,28 +199,28 @@ const DetailPage = () => {
 
               {/** OverView */}
               <div className="w-full">
-                <S.SubText>OverView.</S.SubText>
-                <S.OverViewText>{dataById[parsedId].overView}</S.OverViewText>
+                <SubText>OverView.</SubText>
+                <OverViewText>{dataById[parsedId].overView}</OverViewText>
               </div>
               {/** Info */}
               <div className="mb-28 mt-16 w-full">
-                <S.SubText>Info.</S.SubText>
+                <SubText>Info.</SubText>
                 <div className="ml-4 flex flex-col gap-3">
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 기간</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[0]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 구성원</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[1]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 업무</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[2]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>기술 스택</S.InfoTitle>
-                    <S.InfoText>
+                  <InfoContainer>
+                    <InfoTitle>개발 기간</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[0]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>개발 구성원</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[1]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>개발 업무</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[2]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>기술 스택</InfoTitle>
+                    <InfoText>
                       {dataById[parsedId].infoList[3].map((item, index) => (
                         <Image
                           key={index}
@@ -90,41 +229,38 @@ const DetailPage = () => {
                           className="rounded-xl"
                         />
                       ))}
-                    </S.InfoText>
-                  </S.InfoContainer>
+                    </InfoText>
+                  </InfoContainer>
                 </div>
               </div>
               <div className="mt-10">
-                <S.Link onClick={() => router.back()} className="px-14 py-6">
+                <Link onClick={() => router.back()} className="px-14 py-6">
                   목록으로 가기
-                </S.Link>
+                </Link>
               </div>
             </div>
           )
         : dataById[parsedId] && (
             <div className="flex w-[44%] flex-col items-center justify-center">
               <div className="flex w-full flex-col gap-2">
-                <S.Title>{dataById[parsedId].title}</S.Title>
-                <S.SubTitle>{dataById[parsedId].subTitle}</S.SubTitle>
-                <S.TagList>
+                <Title>{dataById[parsedId].title}</Title>
+                <SubTitle>{dataById[parsedId].subTitle}</SubTitle>
+                <TagList>
                   {dataById[parsedId].tagList.map((item, index) => (
-                    <S.TagItem key={index}>{`#${item}`}</S.TagItem>
+                    <TagItem key={index}>{`#${item}`}</TagItem>
                   ))}
-                </S.TagList>
+                </TagList>
               </div>
               <div className="mb-8 mt-24 flex w-full items-center justify-center gap-4">
-                <S.Link
+                <Link
                   href={dataById[parsedId].githubURL}
                   className="px-10 py-4"
                 >
                   깃허브 바로가기
-                </S.Link>
-                <S.Link
-                  href={dataById[parsedId].siteURL}
-                  className="px-10 py-4"
-                >
+                </Link>
+                <Link href={dataById[parsedId].siteURL} className="px-10 py-4">
                   사이트 바로가기
-                </S.Link>
+                </Link>
               </div>
 
               <Image
@@ -136,28 +272,28 @@ const DetailPage = () => {
 
               {/** OverView */}
               <div className="w-full">
-                <S.SubText>OverView.</S.SubText>
-                <S.OverViewText>{dataById[parsedId].overView}</S.OverViewText>
+                <SubText>OverView.</SubText>
+                <OverViewText>{dataById[parsedId].overView}</OverViewText>
               </div>
               {/** Info */}
               <div className="mb-28 mt-16 w-full">
-                <S.SubText>Info.</S.SubText>
+                <SubText>Info.</SubText>
                 <div className="ml-4 flex flex-col gap-3">
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 기간</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[0]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 구성원</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[1]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>개발 업무</S.InfoTitle>
-                    <S.InfoText>{dataById[parsedId].infoList[2]}</S.InfoText>
-                  </S.InfoContainer>
-                  <S.InfoContainer>
-                    <S.InfoTitle>기술 스택</S.InfoTitle>
-                    <S.InfoText>
+                  <InfoContainer>
+                    <InfoTitle>개발 기간</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[0]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>개발 구성원</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[1]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>개발 업무</InfoTitle>
+                    <InfoText>{dataById[parsedId].infoList[2]}</InfoText>
+                  </InfoContainer>
+                  <InfoContainer>
+                    <InfoTitle>기술 스택</InfoTitle>
+                    <InfoText>
                       {dataById[parsedId].infoList[3].map((item, index) => (
                         <Image
                           key={index}
@@ -166,14 +302,14 @@ const DetailPage = () => {
                           className="rounded-xl"
                         />
                       ))}
-                    </S.InfoText>
-                  </S.InfoContainer>
+                    </InfoText>
+                  </InfoContainer>
                 </div>
               </div>
               <div className="mt-10">
-                <S.Link onClick={() => router.back()} className="px-14 py-6">
+                <Link onClick={() => router.back()} className="px-14 py-6">
                   목록으로 가기
-                </S.Link>
+                </Link>
               </div>
             </div>
           )}
