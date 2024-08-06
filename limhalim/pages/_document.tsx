51029962a4c1e2972102f027b926a3077d4ba8cd
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-08-06 16:54:07 +0900

    design: open graph 설정

--- limhalim/pages/_document.tsx
+++ limhalim/pages/_document.tsx
@@ -3,38 +3,57 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/** opengraph 설정 */}
+        <meta property="og:title" content="Limhalim Portfolio" />
+        <meta
+          property="og:url"
+          content="https://limhalim-portfolio.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/TitleImage.png" />
+        <meta property="og:description" content="임하림의 웹 포트폴리오" />
+      </Head>
+
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+      {/* Preloading fonts */}
+      <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Noto+Sans+KR:wght@500&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+        as="style"
+      />
+      <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700&display=swap"
+        as="style"
+      />
+      <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700;800&display=swap"
+        as="style"
+      />
+
+      {/* Using font-display swap */}
       <link
         href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Noto+Sans+KR:wght@500&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap"
         rel="stylesheet"
-      ></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Libre+Barcode+128+Text&display=swap"
-        rel="stylesheet"
-      ></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Rubik+Burned&display=swap"
-        rel="stylesheet"
-      ></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
-        rel="stylesheet"
-      ></link>
-
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700&display=swap"
         rel="stylesheet"
-      ></link>
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700;800&display=swap"
         rel="stylesheet"
-      ></link>
+      />
+
+      {/* Additional fonts from Naver */}
       <link
         href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square.css"
         rel="stylesheet"
-      ></link>
+      />
       <body>
         <Main />
         <NextScript />
