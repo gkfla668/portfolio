4281e7f6adfce27d365be626a32fe883b7e1840f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-08-08 03:20:10 +0900

    feat: title 요소 추가

--- limhalim/pages/_document.tsx
+++ limhalim/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Limhalim Portfolio</title>
       <Head>
         {/** opengraph 설정 */}
         <meta property="og:title" content="Limhalim Portfolio" />
