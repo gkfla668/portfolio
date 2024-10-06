79f8a5558cf2bf208f7a7ebfae476ad0eabae1fb
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-10-06 13:48:55 +0900

    fix: vercel analytics 태그 위치 변경

--- limhalim/pages/_app.tsx
+++ limhalim/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import "../components/Slide/slick.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
+      <Analytics />
     </RecoilRoot>
   );
 }
