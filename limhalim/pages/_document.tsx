e0df89b043cb9a8ec04d0f4d1c1b8d94c5c309ee
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-10-13 22:49:21 +0900

    feat: seo 점수 높이기, description meta tag 추가

--- limhalim/pages/_document.tsx
+++ limhalim/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
       <title>Limhalim Portfolio</title>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="임하림의 프론트엔드 웹 포트폴리오, 지금 확인해보세요."
+        />
+
         {/** opengraph 설정 */}
         <meta property="og:title" content="Limhalim Portfolio" />
         <meta
