ef6cf924d7b13e957baafd4598eaf49b31e25f16
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-10-15 17:38:48 +0900

    design: 스킬 페이지 수정

--- limhalim/components/Skills/index.tsx
+++ limhalim/components/Skills/index.tsx
@@ -1,73 +1,103 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import HandleScroll from "@/utils/handleScroll";
 
+import HTMLSVG from "../../public/icons/HTML.svg";
+import CSSSVG from "../../public/icons/CSS3_logo.svg";
 import ReactSVG from "../../public/icons/react_circle.png";
 import NextJSSVG from "../../public/icons/next.js_circle.png";
 import JavaScriptSVG from "../../public/icons/javascript_circle.svg";
 import TypeScriptSVG from "../../public/icons/typescript_circle.png";
 import RecoilSVG from "../../public/icons/recoil_circle.png";
-import ReduxSVG from "../../public/icons/redux_circle.webp";
-import TailwindSVG from "../../public/icons/tailwind_circle.png";
-import StyledComponentsSVG from "../../public/icons/styledcomponents_circle.png";
 
 import * as S from "./styled";
+import useScrollHandler from "../../hooks/useScrollHandler";
 
 const Skills = () => {
-  const [scroll, setScroll] = useState<boolean>(false);
-  const [stack, setStack] = useState<string>("Front-end");
-
-  useEffect(() => {
-    setStack("Front-end");
-
-    const threshold = 1000;
-    const scrollHandler = () => HandleScroll({ threshold, setScroll });
-
-    window.addEventListener("scroll", scrollHandler);
-    return () => {
-      window.removeEventListener("scroll", scrollHandler); //clean up
-    };
-  }, [scroll]);
+  const scroll = useScrollHandler(1000);
 
   return (
     <S.Container id="Skills">
       {scroll && (
         <S.ContentBox>
-          <S.StackCircle>
-            <S.Center>{stack}</S.Center>
-            <S.OutCircle>
-              <S.Item onClick={() => setStack("React")}>
-                <Image src={ReactSVG} alt="react" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("NextJS")}>
-                <Image src={NextJSSVG} alt="nextjs" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("JavaScript")}>
-                <Image src={JavaScriptSVG} alt="javascript" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("TypeScript")}>
-                <Image src={TypeScriptSVG} alt="typescript" loading="lazy" />
-              </S.Item>
-            </S.OutCircle>
-            <S.InCircle>
-              <S.Item onClick={() => setStack("Recoil")}>
-                <Image src={RecoilSVG} alt="recoil" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("Redux-toolkit")}>
-                <Image src={ReduxSVG} alt="redux" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("TailwindCSS")}>
-                <Image src={TailwindSVG} alt="tailwind" loading="lazy" />
-              </S.Item>
-              <S.Item onClick={() => setStack("Styled-components")}>
-                <Image
-                  src={StyledComponentsSVG}
-                  alt="styled-components"
-                  loading="lazy"
-                />
-              </S.Item>
-            </S.InCircle>
-          </S.StackCircle>
+          <S.Item>
+            <S.Stack>
+              <Image
+                src={HTMLSVG}
+                alt="html"
+                loading="lazy"
+                width={120}
+                height={120}
+              />
+            </S.Stack>
+            <S.Box>
+              웹 표준과 접근성을 준수하며, 시맨틱 태그를 사용하여
+              SEO(검색엔진최적화)를 고려합니다.{" "}
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image
+                src={CSSSVG}
+                alt="css"
+                loading="lazy"
+                width={120}
+                height={120}
+              />
+            </S.Stack>
+            <S.Box>
+              각 속성의 성격과 동작원리를 이해하며 사용하고 있습니다. 미디어
+              쿼리를 통한 반응형 웹 디자인을 구현할 수 있습니다.
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image src={JavaScriptSVG} alt="javascript" loading="lazy" />
+            </S.Stack>
+            <S.Box>
+              ES6 문법을 사용하여 코드를 작성할 수 있으며, 이벤트 처리와 비동기
+              처리를 다룰 수 있습니다.
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image src={TypeScriptSVG} alt="typescript" loading="lazy" />
+            </S.Stack>
+            <S.Box>
+              정적 타입을 활용하여 코드의 명확성과 안정성을 높이고 있습니다.
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image src={ReactSVG} alt="react" loading="lazy" />
+            </S.Stack>
+            <S.Box>
+              상태 관리와 생명주기 메서드를 적절히 활용하며, 다양한 Hook의 동작
+              원리를 이해하고 사용하고 있습니다.
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image src={NextJSSVG} alt="nextjs" loading="lazy" />
+            </S.Stack>
+            <S.Box>
+              버사이드 렌더링(SSR)과 정적 생성(SSG) 기능을 활용해 성능을
+              최적화하고 있습니다.
+            </S.Box>
+          </S.Item>
+
+          <S.Item>
+            <S.Stack>
+              <Image src={RecoilSVG} alt="recoil" loading="lazy" />
+            </S.Stack>
+            <S.Box>
+              컴포넌트 간 상태를 효율적으로 관리하고 비동기 상태의 흐름을
+              제어하는 데 익숙합니다.
+            </S.Box>
+          </S.Item>
         </S.ContentBox>
       )}
     </S.Container>
--- limhalim/components/Skills/styled.ts
+++ limhalim/components/Skills/styled.ts
@@ -1,91 +1,67 @@
-import {
-  CircleRotate,
-  Rotate,
-  RotateReverse,
-  fadeIn,
-} from "@/styles/animation";
+import { fade, fadeIn, RotateReverse } from "@/styles/animation";
 import styled from "styled-components";
 
 export const Container = styled.div`
   /* position: relative; */
 
+  width: 100%;
   height: 100%;
 `;
 
 export const ContentBox = styled.div`
+  margin-top: 1.6rem;
+
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-`;
-
-export const StackCircle = styled.div`
-  position: relative;
-
-  width: 480px;
-  height: 480px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  animation: ${fadeIn} 6s forwards;
-
-  @media (max-width: 768px) {
-    width: 240px;
-    height: 240px;
-  }
-`;
-
-export const OutCircle = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-  border: 3px solid #42d17d;
-  box-shadow: 0 0 4px #eee;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  animation: ${Rotate} 32s linear infinite;
+  align-items: start;
+  gap: 0.6rem;
 
   :nth-child(1) {
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    animation: ${fadeIn} 2s forwards;
   }
   :nth-child(2) {
-    top: 50%;
-    right: 0;
-    transform: translate(50%, -50%);
+    animation: ${fadeIn} 2.2s forwards;
   }
   :nth-child(3) {
-    bottom: 0;
-    left: 50%;
-    transform: translate(-50%, 50%);
+    animation: ${fadeIn} 2.4s forwards;
   }
   :nth-child(4) {
-    top: 50%;
-    left: 0;
-    transform: translate(-50%, -50%);
+    animation: ${fadeIn} 2.6s forwards;
+  }
+  :nth-child(5) {
+    animation: ${fadeIn} 2.8s forwards;
+  }
+  :nth-child(6) {
+    animation: ${fadeIn} 3s forwards;
+  }
+  :nth-child(7) {
+    animation: ${fadeIn} 3.2s forwards;
   }
 `;
 
 export const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const Stack = styled.div`
   cursor: pointer;
 
-  width: 72px;
-  height: 72px;
-  position: absolute;
+  width: 68px;
+  height: 68px;
   border-radius: 50%;
-  border: 1px solid white;
+  border: 1px solid #42d17e29;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-image: linear-gradient(180deg, #42d17d 0%, #47c07a 100%);
+  background-image: linear-gradient(180deg, #42d17e29 0%, #42d17e29 100%);
   box-shadow: 0 0 4px #eee;
+
+  box-shadow: 0 0 4px #42d17e29;
 
   img {
     margin: 0px auto;
@@ -107,54 +83,12 @@ export const Item = styled.div`
   }
 `;
 
-export const InCircle = styled.div`
-  animation: ${CircleRotate} 32s linear infinite;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) rotate(45deg);
-  width: 68%;
-  height: 68%;
-  border: 3px solid #42d17d;
-  box-shadow: 0 0 4px #eee;
-  border-radius: 50%;
-
-  :nth-child(1) {
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  :nth-child(2) {
-    top: 50%;
-    right: 0;
-    transform: translate(50%, -50%);
-  }
-  :nth-child(3) {
-    bottom: 0;
-    left: 50%;
-    transform: translate(-50%, 50%);
-  }
-  :nth-child(4) {
-    top: 50%;
-    left: 0;
-    transform: translate(-50%, -50%);
-  }
-`;
-
-export const Center = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
+export const Box = styled.span`
   color: #42d17d;
-  background-color: #42d17e29;
   font-size: 1.6rem;
-  font-weight: 900;
+  font-weight: 600;
   white-space: nowrap;
   text-align: center;
-  border-radius: 2.4rem;
-  padding: 1rem 2.8rem;
 
   @media (max-width: 768px) {
     font-size: 1.3rem;
